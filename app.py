6d4bcf1abe6b416ddd86c6b5fedcb1c7787bd0bc
Zeke Her <herezem@dunwoody.edu>
2025-11-24 13:02:54 -0600

    Inital commit. Site is up and running.

---
+++ app.py
@@ -0,0 +1,25 @@
+#This is the final blog project.
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
+
+app = Flask(__name__)
+# app.secret_key = "NEVERTELLANYONETHISKEY"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ZekeFinalBlog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    # title = db.Column()
+
+# class 
+@app.route('/')
+def mainpage():
+    return redirect(url_for("home"))
+
+@app.route('/home')
+def home():
+    return render_template("blog.html")
