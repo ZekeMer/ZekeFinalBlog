4bb123375dee2aabd44a7ef009c6a7ec0f43987a
Zeke Her <herezem@dunwoody.edu>
2025-11-24 15:01:41 -0600

    Added ignore and slight modifications for security.

--- app.py
+++ app.py
@@ -2,9 +2,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-# app.secret_key = "NEVERTELLANYONETHISKEY"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ZekeFinalBlog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
