eb937907483855a17b653fb65d2e06febd4a4e45
Zeke Her <herezem@dunwoody.edu>
2025-11-26 18:27:59 -0600

    Logins and accounts working at a base level.

--- app.py
+++ app.py
@@ -16,17 +16,24 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class Blog(db.Model):
+class Blog(db.Model): # user.blog --> blogs  user.Author --> author
     BlogId = db.Column(db.Integer, primary_key = True)
     BlogTitle = db.Column(db.String(60), nullable = False)
     BlogAuthor = db.Column(db.String(40), nullable = False)
+    InitalComment = db.Column(db.String(250), nullable = False)
     PostTime = db.Column(db.DateTime, default = datetime.now(timezone.utc))
     Comment = db.Column(db.String(250), nullable = False)
+    UserId = db.Column(db.Integer, db.ForeignKey('user.UserId'), nullable = False) # One-to-Many relationship where one user can have many blogs
 
 class User(db.Model, UserMixin):
     UserId = db.Column(db.Integer, primary_key = True)
     UserName = db.Column(db.String(20), nullable = False, unique = True)
-    Password = db.Column(db.String(20), nullable = False)
+    Password = db.Column(db.String(200), nullable = False)
+    Is_Admin = db.Column(db.Boolean, nullable=False) # Is this user an admin? Try to get this to work via normal sign up and jsut check permissions before sensitive actions.
+    blogs = db.relationship('Blog', backref = "Author", lazy = True) # Check backref len(user.blogs)
+
+    def get_id(self):
+        return str(self.UserId)
 
 with app.app_context():
     db.create_all()
@@ -36,12 +43,28 @@ login_manager.login_view = "login" # return to login if authentication fails
 login_manager.refresh_view = "login" # This does the same as code above, but when session expires. Redundant.
 login_manager.init_app(app) # Configure for login
 
-login_manager.login_message = "To see this page, please log in or create an account."
+login_manager.login_message = "To see page, please log in or create an account."
 
 @login_manager.user_loader #Find correct user
 def load_user(UserId): #Passing arbitrary argument
     #Database lookup logic to query for UserId
-    return User.get(int(UserId))
+    return User.query.get(int(UserId)) # Could be user.UserId or only 'UserId' inside parameter.
+
+@app.cli.command()
+def create_admin():
+    # from werkzeug.security import generate_password_hash
+    # import getpass
+    
+    #Make an admin from the CLI (command line)
+    user_name = input("Enter admin username: ") 
+    pass_word = input("Enter admin password: ")
+    
+    admin = User(UserName=user_name.strip(), Password=pass_word.strip(), Is_Admin=True) #Figure out the tables in here.
+
+    db.session.add(admin)
+    db.session.commit()
+
+    print(f"Admin user {user_name} initiated")
 
 @app.route('/') # This routejust points to the home route below it
 def mainpage():
@@ -49,65 +72,70 @@ def mainpage():
 
 @app.route('/home') 
 def home():
+    # blog = Blog.query.all()
     return render_template("home.html")
 
-@app.route('/admin') # HTML page for route to be added.
-def admin():
-    blog = Blog.query.all()
-    user = User.query.all()
-    return render_template("admin_home.html", blog = blog, user = user)
+# @app.route('/admin') # HTML page for route to be added.
+# def admin():
+    # blog = Blog.query.all()
+    # user = User.query.all()
+    # return render_template("admin.html", blog = blog, user = user)
 
 @app.route('/signup', methods = ['GET', 'POST'])
 def signup():
     if request.method == "POST":
-        UserName = request.form.get("UserName").strip()
-        Password = request.form.get("Password").strip()
+        username = request.form.get("UserName").strip()
+        password = request.form.get("Password").strip()
         confirmPassword = request.form.get("Password").strip()
 
-        username_taken = User.query.filter_by(UserName = UserName)
-
-        if username_taken:
-            return "This username is taken, try another one."
-        elif not UserName:
-            return "This username is invalid! Re-enter a username."
-        elif Password != confirmPassword:
-            return "The passwords do not match. Re-enter them."
-        elif not Password:
-            return "This username is invallid! Re-enter a password."
-        elif len(UserName) < 1: # username needs to be at least 3 characters, length starts at char 0.
-            return "The Username needs to be longer!"
-        elif len(Password) < 8: # password needs to be at least 10 characters
-            return "The password needs to be longer!"
+        if len(username) < 2:
+            flash("The username needs to be at least 2 characters!")
+        elif len(password) < 8:
+            flash("The password needs to be at least 8 characters!")
+        elif not username:
+            flash("This username is invalid! Re-enter the username.")
+        elif not password:
+            flash("This password is invalid! Re-enter a password.")
+        elif password != confirmPassword:
+            flash("The passwords do not match. Re-enter them.")
         else:
-            try:
-                new_user = User(UserName = UserName, Password = Password)
-                db.session.add(new_user)
-                db.session.commit()
-                login_user(new_user, remember = False, fresh = True) # include the user object, remember after session expires? = NO, this is a fresh sign in.
-                return redirect(url_for('login.html')) # GO TO THE LOGIN PAGE
-            except Exception as e: 
-                db.session.rollback()
-                signUpError = "There was an issue signing up, please try again."
-                return render_template('signup.html', signUpError = signUpError)
+            username_taken = User.query.filter(User.UserName == username).first()
+            if username_taken:
+                flash("This username is taken, try another.")
+            else:
+                try:
+                    new_user = User(UserName = username, Password = password, Is_Admin = False) # This should be set to false forever.
+                    db.session.add(new_user)
+                    db.session.commit()
+                    login_user(new_user, remember = False, fresh = True) # include the user object, remember after session expires? = NO, this is a fresh sign in.
+                    flash("Sign up successful. Log in.")
+                    return redirect(url_for('login')) # GO TO THE LOGIN PAGE and sign in with the credentials they just made. 
+                except Exception as e: 
+                    db.session.rollback()
+                    signUpError = "There was an issue signing up, please try again."
+                    return render_template('signup.html', signUpError = signUpError)
     
     return render_template('signup.html')
 
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-    if request.method == "POSt":
-        UserName = request.form.get("UserName").strip()
-        Password = request.form.get("Password").strip()
+    if request.method == "POST":
+        username = request.form.get("UserName").strip()
+        password = request.form.get("Password").strip()
 
-        userFound = User.query.filter_by(UserName = UserName)
+        userFound = User.query.filter(User.UserName == username).first() #Proper way to check
         if userFound:
-            if User.Password == Password:
+            if userFound.Password == password: 
                 login_user(userFound, remember = False, fresh = True)
-                return redirect(url_for('home.html'))
+                return redirect(url_for('home'))
             else:
-                return "Wrong Password. Try again."
+                flash("Wrong Password. Try again.")
+                return render_template("login.html")
+
         else: 
-            return "This username doesn't exist. Re-enter it or sign up."
+            flash("This username doesn't exist. Re-enter it or sign up.") 
+            return render_template("login.html")
         
     return render_template("login.html")
 
@@ -115,9 +143,9 @@ def login():
 @login_required # Require login to take action or see page - In this case, the user cannot logout if they don't have an account
 def logout():
     logout_user()
-    return redirect(url_for('home.html')) # This is a guest, figure it out!
+    return redirect(url_for('login')) # This is a guest, figure it out!
 
-@app.route('/home/post') # This route marks the start of the site's actual pages.
+@app.route('/home/post') # This route marks the start of the site's actual pages. Have a user make a post
 def home_post():
     pass
 
